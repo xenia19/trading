82607bc03ffabfbea81c5fc97c651fd2dde79838
Xenia <52275879+xenia19@users.noreply.github.com>
2023-03-08 19:50:49 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def run_colab():
-    gdown.download('https://colab.research.google.com/drive/1cqtAqgelwaZnvr80DJIb9UF_MGs-NRo0', 'colab.ipynb', quiet=False)
+    gdown.download('https://drive.google.com/drive/folders/1xQFZ5IPjVvz1qMBZGchYWQ6qyxLKetLj', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
